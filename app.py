1639217ea9ef8fc1deb8ee92fa7ab14b34acec90
saralafia <sara.lafia@gmail.com>
2020-05-13 21:07:05 -0700

    Initializing repo
    Linking to heroku

---
+++ app.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import plotly.express as px
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objects as go
+from dash.dependencies import Input, Output
+
+df = pd.read_csv('data.csv', index_col=0)
+df.head()
+mytitle='ERI projects & publications (2009 - 2019) clustered by topic'
+tabtitle='ERI Research Map'
+githublink='https://github.com/saralafia'
+
+fig = px.scatter(df, 
+                 x=df.x, 
+                 y=df.y,
+                 color=df.main_label, 
+                 hover_data={'x':False,
+                             'y':False, 
+                             'title':df.title,
+                             'authors':df.authors,
+                             'identifier':df.identifier,
+                             'year':df.year, 
+                             'type':df.type, 
+                             'topic_keywords':df.main_keys, 
+                             'main_label':False},
+                 color_discrete_sequence=px.colors.qualitative.D3,
+                 #title=mytitle,
+                 template='plotly_white')
+
+fig.update_layout(xaxis_showgrid=False, 
+                  yaxis_showgrid=False, 
+                  xaxis_zeroline=False, 
+                  yaxis_zeroline=False, 
+                  yaxis_visible=False, 
+                  xaxis_visible=False)
+
+fig.update_layout(height=800, width=1200, legend_title='Main Topic', legend= {'itemsizing': 'constant'})
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.title=tabtitle
+
+app.layout = html.Div(children=[
+    html.H1(mytitle), 
+    html.A('code + data', href=githublink, target="_blank"),
+    html.Br(),
+    dcc.Graph(
+        id='ERI-map',
+        figure=fig
+    ), 
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=False)
